a0c551f3b294edf49f00d312a463d6e102cbaa7f
Kirstóf Umann <dkszelethus@gmail.com>
2021-04-08 17:56:46 +0200

    Experimenting with a native representation of port numbers in main.py

--- Node.py
+++ Node.py
@@ -18,14 +18,15 @@ class Graph:
         self.ports = []
 
     def encode(self, oracle_type, robot_pos):
-        if oracle_type == MAP_ORACLE:
+        if oracle_type == INSTANCE_ORACLE:
             edges = nx.dfs_labeled_edges(minimum_spanning_tree(self.nx_graph), robot_pos)
         else:
-            edges = nx.dfs_labeled_edges(minimum_spanning_tree(self.nx_graph), random.randint(1, self.nx_graph.number_of_nodes()))
+            #edges = nx.dfs_labeled_edges(minimum_spanning_tree(self.nx_graph), random.randint(1, self.nx_graph.number_of_nodes()))
+            # Lets not make it random, otherwise it wouldn't be deterministic
+            edges = nx.dfs_labeled_edges(minimum_spanning_tree(self.nx_graph), 1)
 
         visited_nodes = 0
         bit = math.ceil(math.log(self.nx_graph.number_of_nodes(), 2))
-        print('bit:', bit)
         for u, v, d in edges:
             found = False
             if visited_nodes != self.nx_graph.number_of_nodes():
@@ -36,7 +37,7 @@ class Graph:
                     for nod in self.node_index:
                         k = 0
                         for por in nod.ports:
-                            if u == por.n1 and v == por.n2:
+                            if u == por.to_node and v == por.on_port:
                                 self.ports.append(get_bin(k, bit))
                                 found = True
                             k += 1
@@ -48,7 +49,7 @@ class Graph:
                     for nod in reversed(self.node_index):
                         k = 0
                         for por in nod.ports:
-                            if u == por.n1 and v == por.n2:
+                            if u == por.to_node and v == por.on_port:
                                 self.ports.append(get_bin(k, bit))
                                 found = True
                             k += 1
@@ -56,6 +57,9 @@ class Graph:
                             break
             elif d == 'reverse':
                 self.back_source.append(0)
+
+        # The first 1 seems to be excessive, idk why.
+        self.path.pop(0)
 
         return ''.join(str(x) for x in self.path + self.back_source + self.ports)
 
@@ -79,15 +83,14 @@ class Node:
     def to_string(self):
         print("id:", self.id, "ports:", self.ports)
 
-
 class Port:
-    def __init__(self, n1, n2):
-        self.n1 = n1
-        self.n2 = n2
+    def __init__(self, to_node, on_port):
+        self.to_node = to_node
+        self.on_port = on_port
         self.taken = False
 
     def to_string(self):
-        print("n1:", self.n1, "n2:", self.n2, "taken: ", self.taken)
+        print("n1:", self.to_node, "n2:", self.on_port, "taken: ", self.taken)
 
     def equals(self, other):
-        return other is not None and self.n1 == other.n1 and self.n2 == other.n2
+        return other is not None and self.to_node == other.to_node and self.on_port == other.on_port
--- main.py
+++ main.py
@@ -13,60 +13,44 @@ from collections import defaultdict
 def get_bin(x, n=0):
     return format(x, 'b').zfill(n)
 
+r1 = dict( name  = 'R1', ports = [0, 1] )
+r2 = dict( name  = 'R2', ports = [0, 1] )
+r3 = dict( name  = 'R3', ports = [0, 1] )
+
+routers = [r1,r2,r3]
 
 G = nx.Graph()
 
-G.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24])
-G.add_edges_from([(1, 2), (1, 3), (1, 4),
-                  (2, 3), (2, 4),
-                  (3, 4),
-                  (4, 5), (4, 6), (4, 7), (4, 8),
-                  (5, 6), (5, 7), (5, 8),
-                  (6, 7), (6, 8), (6, 9), (6, 10),
-                  (7, 11), (7, 12),
-                  (9, 10),
-                  (11, 12),
-                  (12, 13), (12, 14), (12, 15),
-                  (14, 16), (14, 17),
-                  (15, 18), (15, 21),
-                  (16, 17),
-                  (18, 19), (18, 20),
-                  (21, 22), (21, 23), (21, 24),
-                  (22, 23), (22, 24),
-                  (23, 24)])
+for r in routers:
+  # Add ports as attributes
+  G.add_node(r['name'], name=r['name'], ports=r['ports'])
 
-nx.draw(minimum_spanning_tree(G), with_labels=True, font_weight='bold')
-#plt.show()
+G.nodes().get('R3', None)
 
-print('Number of Nodes:', G.number_of_nodes())
+def add_edge_port(G, node1, port1, node2, port2):
+  node_list = [node1, node2]
+  port_list = [port1, port2]
 
-nodes = [Node(1, [Port(1, 2), Port(1, 3), Port(1, 4)]),
-         Node(2, [Port(1, 2), Port(2, 3), Port(2, 4)]),
-         Node(3, [Port(1, 3), Port(2, 3), Port(3, 4)]),
-         Node(4, [Port(1, 4), Port(2, 4), Port(3, 4), Port(4, 5), Port(4, 6), Port(4, 7), Port(4, 8)]),
-         Node(5, [Port(4, 5), Port(5, 6), Port(5, 7), Port(5, 8)]),
-         Node(6, [Port(4, 6), Port(5, 6), Port(6, 7), Port(6, 8), Port(6, 9), Port(6, 10)]),
-         Node(7, [Port(4, 7), Port(5, 7), Port(6, 7), Port(7, 8), Port(7, 11), Port(7, 12)]),
-         Node(8, [Port(4, 8), Port(5, 8), Port(6, 8), Port(7, 8)]),
-         Node(9, [Port(6, 9), Port(9, 10)]),
-         Node(10, [Port(6, 10), Port(9, 10)]),
-         Node(11, [Port(7, 11), Port(11, 12)]),
-         Node(12, [Port(6, 12), Port(11, 12), Port(12, 13), Port(12, 14), Port(12, 15)]),
-         Node(13, [Port(12, 13)]),
-         Node(14, [Port(12, 14), Port(14, 16), Port(14, 17)]),
-         Node(15, [Port(12, 15), Port(15, 18), Port(15, 21)]),
-         Node(16, [Port(14, 16), Port(16, 17)]),
-         Node(17, [Port(14, 17), Port(16, 17)]),
-         Node(18, [Port(15, 18), Port(18, 19), Port(18, 20)]),
-         Node(19, [Port(18, 19)]),
-         Node(20, [Port(18, 20)]),
-         Node(21, [Port(15, 21), Port(21, 22), Port(21, 23), Port(21, 24)]),
-         Node(22, [Port(21, 22), Port(22, 23), Port(22, 24)]),
-         Node(23, [Port(21, 23), Port(22, 23), Port(23, 24)]),
-         Node(24, [Port(21, 23), Port(22, 24), Port(23, 24)])]
+  edge_ports = []
 
-NewGraph = Graph(G, nodes)
+  for idx in range(0, 2):
+    node_idx = node_list[idx]
+    port_idx = port_list[idx]
 
-robot_pos = 1
+    # Sanity check to see if the nodes and ports are present in Graph
+    if G.nodes().get(node_idx, None) is None:
+      print("Node : {} is not present in Graph".format(node_idx))
+      return
 
-print(NewGraph.encode(MAP_ORACLE, robot_pos))
+    if G.nodes(data=True)[node_idx]['ports'][port_idx] is None:
+      print("Port ID :{} is incorrect for Node ID : {}!".
+            format(node_idx, port_idx))
+      return
+
+    edge_ports.append(node_idx + '.' + str(port_idx))
+
+  # Add the anchor points as edge attributes
+  G.add_edge(node1, node2, anchors=edge_ports)
+
+add_edge_port(G, 'R1', 1, 'R2', 0)
+print(nx.get_edge_attributes(G, 'anchors'))
