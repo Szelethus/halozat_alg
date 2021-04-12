36fd3ab301791f7cde0bf26ed4ea4068f5c8e930
Kirstóf Umann <dkszelethus@gmail.com>
2021-04-12 17:49:31 +0200

    Add back the big example without any testing yet

--- tests.py
+++ tests.py
@@ -124,5 +124,72 @@ class TestSum(unittest.TestCase):
 
 
 
+    def test_big_graphself):
+        GraphToEncode = Graph()
+        GraphToEncode.init_with_dicts(24, [
+            dict(n1=0, p1=0, n2=1, p2=1),
+            dict(n1=0, p1=1, n2=3, p2=1),
+            dict(n1=0, p1=2, n2=2, p2=0),
+
+            dict(n1=1, p1=2, n2=2, p2=1),
+            dict(n1=1, p1=0, n2=3, p2=3),
+
+            dict(n1=2, p1=2, n2=3, p2=0),
+
+            dict(n1=3, p1=2, n2=4, p2=1),
+            dict(n1=3, p1=4, n2=5, p2=3),
+            dict(n1=3, p1=6, n2=9, p2=2),
+            dict(n1=3, p1=5, n2=8, p2=0),
+
+            dict(n1=4, p1=0, n2=5, p2=1),
+            dict(n1=4, p1=3, n2=9, p2=1),
+            dict(n1=4, p1=2, n2=8, p2=1),
+
+            dict(n1=5, p1=2, n2=6, p2=0),
+            dict(n1=5, p1=4, n2=7, p2=0),
+            dict(n1=5, p1=0, n2=8, p2=2),
+            dict(n1=5, p1=5, n2=9, p2=0),
+
+            dict(n1=6, p1=1, n2=7, p2=1),
+
+            dict(n1=8, p1=3, n2=9, p2=4),
+
+            dict(n1=9, p1=5, n2=10, p2=0),
+            dict(n1=9, p1=6, n2=11, p2=3),
+
+            dict(n1=10, p1=1, n2=11, p2=0),
+
+            dict(n1=11, p1=2, n2=12, p2=0),
+            dict(n1=11, p1=4, n2=13, p2=2),
+            dict(n1=11, p1=1, n2=16, p2=0),
+
+            dict(n1=13, p1=0, n2=14, p2=0),
+            dict(n1=13, p1=1, n2=15, p2=0),
+
+            dict(n1=14, p1=1, n2=15, p2=1),
+
+            dict(n1=16, p1=2, n2=17, p2=1),
+            dict(n1=16, p1=1, n2=20, p2=3),
+
+            dict(n1=17, p1=2, n2=18, p2=0),
+            dict(n1=17, p1=0, n2=19, p2=0),
+
+            dict(n1=20, p1=2, n2=21, p2=1),
+            dict(n1=20, p1=0, n2=22, p2=2),
+            dict(n1=20, p1=1, n2=23, p2=1),
+
+            dict(n1=21, p1=0, n2=22, p2=0),
+            dict(n1=21, p1=2, n2=23, p2=2),
+
+            dict(n1=22, p1=1, n2=23, p2=0),
+        ])
+
+        robot_pos = 0
+        NewGraph = Graph()
+        NewGraph.init_with_decode(GraphToEncode.encode(INSTANCE_ORACLE, robot_pos))
+
+        NewGraph.print_graph()
+        NewGraph.get_edge_labels()
+
 if __name__ == '__main__':
     unittest.main()
